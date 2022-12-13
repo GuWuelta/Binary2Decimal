4743c0921d5c62e18aec932ff5cba7b96f2d1a15
GuWuelta <rm93449@fiap.com.br>
2022-12-13 17:34:31 -0300

    feat: functions returns add

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-function convertBinaryToStringArray(binary: string) {
+function convertBinaryToStringArray(binary: string): string[] {
   const stringArray = binary.replace(".", ",").split("");
   return stringArray;
 }
 
-function catchIdxs(stringArray: string[]) {
+function catchIdxs(stringArray: string[]): number {
   if (!stringArray.includes(",")) return 0;
   const commaPosition = stringArray.indexOf(",");
   const numsAfterComma = stringArray.slice(commaPosition + 1);
@@ -11,16 +11,17 @@ function catchIdxs(stringArray: string[]) {
   return -idxs;
 }
 
-function convertStringArrayToNumberArray(stringArray: string[]) {
+function convertStringArrayToNumberArray(stringArray: string[]): (number | undefined)[] {
   let numberArray = stringArray.map((element) => {
     if (element === "0" || element === "1") return Number(element);
   });
   return numberArray.filter((num) => typeof num === "number");
 }
 
-function calculate(numberArray: number[], idx: number) {
-  numberArray.reduceRight((acc, cur, idx) => {
+function calculate(numberArray: number[], idx: number): number {
+  return numberArray.reduceRight((acc, cur) => {
     acc += cur * 2 ** idx;
+    idx++;
     return acc;
   }, 0);
 }
