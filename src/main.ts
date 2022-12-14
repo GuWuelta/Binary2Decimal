34d05d32b8f7b6ff9556be17fea910b284d01bdc
GuWuelta <rm93449@fiap.com.br>
2022-12-14 17:33:54 -0300

    refactor: loop for changed for reduce

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 /**
  *  This project serves to receive a binary number and convert it to decimal.
- * 
+ *
  * @author Gustavo Wuelta
  */
 
-
 /**
  * Function responsible for receiving the binary number and transforming it into an array of strings
- * 
+ *
  * @param - binary number in string format
  * --------------------------------
  * @return - array of strings with the characters of the parameter passed
@@ -45,7 +44,7 @@ function checkIsBinary(strings: string[]): boolean | undefined {
 
 /**
  * Function that analyzes whether the binary contains a comma and returns an index that will be used in the potentiation calculation
- * 
+ *
  * @param - array of strings
  * --------------------------------
  * @return - Number: 0 if there is no comma, if there is, returns the quantity of numbers after the comma
@@ -61,7 +60,7 @@ function catchIndexes(strings: string[]): number {
 
 /**
  * Function that turns an array of strings into an array of numbers
- * 
+ *
  * @param - array of strings
  * --------------------------------
  * @return - array of numbers
@@ -69,19 +68,18 @@ function catchIndexes(strings: string[]): number {
  */
 function convertStringsToNumbers(strings: string[]): number[] {
   // Usar Reduce
-  const numbers: number[] = [];
-  for (const string of strings) {
-    // Usar includes
-    if (string === "0" || string === "1") {
-      numbers.push(Number(string));
+  const numbers = strings.reduce((acc: number[], cur) => {
+    if (cur === "0" || cur === "1") {
+      acc.push(Number(cur));
     }
-  }
+    return acc;
+  }, []);
   return numbers;
 }
 
 /**
  * Function that performs the calculation and returns the binary already in decimal format
- * 
+ *
  * @param - array of numbers
  * --------------------------------
  * @return - Number: Binary in decimal format
@@ -97,7 +95,7 @@ function calculate(numbers: number[], idx: number): number {
 
 /**
  * Function responsible for calling all others and running the project
- * 
+ *
  * @param - Binary number in string format
  * --------------------------------
  * @return - void
